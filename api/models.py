ff11995afc86f0a9afe95a23eccecd49d326ccf6
vr3n <vickyspatel@gmail.com>
2021-03-04 14:03:41 +0530

    Created Serializer for Room

--- api/models.py
+++ api/models.py
@@ -2,11 +2,12 @@ from django.db import models
 import string
 import random
 
-
 # Create your models here.
 
-def generate_unique_code():
-    length = 6
+def generate_unique_code(length):
+    """
+    length --> integer: Specify the length of generated code.
+    """
 
     while True:
         code = ''.join(random.choices(string.ascii_uppercase, k=length))
--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
+from api.models import Room
 from django.urls import path, include
-from .views import home
+from .views import RoomView
 
 urlpatterns = [
-    path('', home, name="api_home"),
+    path('', RoomView.as_view(), name="create_rooms"),
 ]
