f4b80edb9e9c486da7d7748dad6ff61751fe4d82
vr3n <vickyspatel@gmail.com>
2021-03-04 16:30:53 +0530

    Created create-room api

--- api/models.py
+++ api/models.py
@@ -4,10 +4,8 @@ import random
 
 # Create your models here.
 
-def generate_unique_code(length):
-    """
-    length --> integer: Specify the length of generated code.
-    """
+def generate_unique_code():
+    length = 6
 
     while True:
         code = ''.join(random.choices(string.ascii_uppercase, k=length))
@@ -17,11 +15,12 @@ def generate_unique_code(length):
     return code
 
 class Room(models.Model):
-    code = models.CharField(max_length=8, default="", unique=True)
+    code = models.CharField(max_length=8, default=generate_unique_code, unique=True)
     host = models.CharField(max_length=50, unique=True)
     guest_can_pause = models.BooleanField(default=False, null=False)
     votes_to_skip = models.IntegerField(null=False, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
         return f"{self.code} - {self.host}"
--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from api.models import Room
 from django.urls import path, include
-from .views import RoomView
+from .views import RoomView, CreateRoomView
 
 urlpatterns = [
-    path('', RoomView.as_view(), name="create_rooms"),
+    path('', RoomView.as_view(), name="list_rooms"),
+    path('create-room/', CreateRoomView.as_view(), name="create_rooms"),
 ]
